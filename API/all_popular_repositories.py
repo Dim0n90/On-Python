13ef39180b214cc230b0977d11194ec63c3e2a9a
Dim0n90 <ya.biletskij@gmail.com>
2022-09-20 13:56:20 +0300

    Added link to pages of owner

--- API/all_popular_repositories.py
+++ API/all_popular_repositories.py
@@ -17,20 +17,28 @@ print(f"Total repositories: {response_dict['total_count']}") # Общее кол
 repos_dicts = response_dict['items'] # это словарь, который содержит инфу про одно отдельное хранилище
 print(f"Repositories returned: {len(repos_dicts)}") # про сколько репов есть инфа
 
-repos_names, stars = [], []
+repos_links, stars, labels = [], [], []
 for details_repos in repos_dicts: # смотрим данные про каждый реп
-    repos_names.append(details_repos['name'])
+    
+    repos_names = details_repos['name']
+    repos_url = details_repos['html_url']
+    repos_link = f"<a href='{repos_url}'>{repos_names}</a>" # ссылка на проэкт
+    repos_links.append(repos_link)
     stars.append(details_repos['stargazers_count'])
 
+    owner = details_repos['owner']['login']
+    desription = details_repos['description'] 
+    label = f"{owner}<br />{desription}" # делаем перенос ряда во всей записи   
+    labels.append(label)
 """Делаем визуализацию"""
 data = [{'type': 'bar',
-        'x': repos_names, 'y': stars,
-        'marker': {'color': 'rgb(60, 100, 150)',
-                   'line': {'width': 1.5, 'color': 'rgb(150, 25, 25)'}},
+        'x': repos_links, 'y': stars, 'hovertext': labels, # делаем ссылку на HTML, подпись курсора 
+        'marker': {'color': 'rgb(50, 75, 50)',
+                   'line': {'width': 1.5, 'color': 'rgb(75, 25, 50)'}},
         'opacity': 0.6}]
 my_layout = {'title': 'Most - Starred Python Projects on GitHub',
-            'titlefont': {'size': 28}, # ШРИФТ
-            'xaxis': {'title': 'Repository','titlefont': {'size': 24}, 'tickfont': {'size':14}},
+            'titlefont': {'size': 28}, # ШРИФТ 
+            'xaxis': {'title': 'Repositories','titlefont': {'size': 24}, 'tickfont': {'size':14}},
             'yaxis': {'title': 'Stars', 'titlefont': {'size': 24}, 'tickfont': {'size':14}}}
 
 fig = {'data': data, 'layout': my_layout}
